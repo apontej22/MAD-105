c84f7cdb5462afc1fc065ae001dc2aa77833c740
apontej22 <japonte@students.mchenry.edu>
2017-08-27 15:59:48 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello! My name is Janet");
+	System.out.println("I am working towards an AAS in Computer Information System.");
+	System.out.println("This is the third programming class that I have taken.");
+	System.out.println("Even though I have not many experience, I like programming.");
+    }
+}
